558b531a6839701501f833199bf3d4925fbc50ff
chenpanpan <13396073613@163.com>
2019-03-31 01:54:34 +0800

    添加springcloud config client获取github上的配置信息

---
+++ config-client/src/main/java/com/panpan/controller/LoadConfigController.java
@@ -0,0 +1,25 @@
+package com.panpan.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by panpan on 2019/3/31.
+ */
+@RestController
+@RequestMapping("config")
+@RefreshScope
+public class LoadConfigController {
+
+    @Value("${name}")
+     String name;
+
+    @GetMapping("/getConfig")
+    public String getConfig(){
+        return this.name;
+    }
+
+}
